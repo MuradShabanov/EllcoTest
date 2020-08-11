553b746e04e5f0d105725990fa2cdf0c15a0a96f
MuradShabanov <murad020406@gmail.com>
2020-08-11 14:42:35 +0300

    commit

--- EllcoTest/CustomCell.swift
+++ EllcoTest/CustomCell.swift
@@ -30,6 +30,7 @@ class CustomCell: UICollectionViewCell {
         configureVersionLable()
         configureSenderLabel()
         
+        layoutIfNeeded()
         configureElements()
     }
     
